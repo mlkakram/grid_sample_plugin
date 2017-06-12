56d456fe78a91b335b193fff5a5a0f9fe026d592
iretiayo <iretibob@gmail.com>
2017-06-12 10:49:34 -0400

    pre_grasp_dofs can be optionally passed in to evaluate grasps. defaults to zero if not passed in which makes most hands fully open. But the fetch gripper 'fully open' gripper pose is greater than 0. [4,] works

--- grid_sample_client/src/grid_sample_client/grid_sample_client.py
+++ grid_sample_client/src/grid_sample_client/grid_sample_client.py
@@ -32,14 +32,17 @@ class GridSampleClient(object):
         return client.get_result()
 
     @classmethod
-    def evaluatePreGrasps(cls, pre_grasps):
+    def evaluatePreGrasps(cls, pre_grasps, pre_grasp_dofs=None):
         import graspit_commander
         gc = graspit_commander.GraspitCommander()
 
         grasps = []
         for i, pre_grasp in enumerate(pre_grasps):
             gc.toggleAllCollisions(False)
-            gc.forceRobotDof(pre_grasp.dofs)
+            if not pre_grasp_dofs:
+                gc.forceRobotDof(pre_grasp.dofs)
+            else:
+                gc.forceRobotDof(pre_grasp_dofs)
             gc.setRobotPose(pre_grasp.pose)
 
             gc.toggleAllCollisions(True)
